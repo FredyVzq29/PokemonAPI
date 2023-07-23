73c9cbb860f4b7df58cb753fa5220868478cc09d
FredyVzq29 <alfredvazq30@gmail.com>
2023-07-23 00:28:46 -0600

    Created events listener and initial methods in MainActivity.java. Added Volley to handle API request

--- app/src/main/java/com/fredy/pokemonconsumer/activities/MainActivity.java
+++ app/src/main/java/com/fredy/pokemonconsumer/activities/MainActivity.java
@@ -1,16 +1,53 @@
-package com.fredy.pokemonconsumer;
+package com.fredy.pokemonconsumer.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.fredy.pokemonconsumer.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button randomPokemon = (Button) findViewById(R.id.random_button); //* Property used to handle clicks of random pokemon button
+    private Button searchPokemon = (Button) findViewById(R.id.search_button); //* Property used to handle clicks of search pokemon button
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupAllProperties();
     }
 
-    
+    /**
+     * This method is used to load and set the properties of the class
+     */
+    private void setupAllProperties() {
+        randomPokemon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                generateRandomPokemonCall();
+            }
+        });
+
+        searchPokemon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToSearchPokemonView();
+            }
+        });
+    }
+
+    /**
+     * Requesting a random pokemon
+     */
+    private void generateRandomPokemonCall() {
+
+    }
+
+    /**
+     * Method used to change page to a search pokemon
+     */
+    private void goToSearchPokemonView() {
+
+    }
 }
